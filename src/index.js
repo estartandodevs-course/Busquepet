44868f0c28b72a0e870f3ecd8e761ecf0658e8cc
Joao Paulo <joaopaulofariadecarvalho@gmail.com>
2020-11-26 22:33:24 -0300

    Adicionado Button

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Button} from './components/button/Button';
 
 ReactDOM.render(
   <React.StrictMode>
