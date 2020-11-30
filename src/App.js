3aac556c1a0a316d713e17fac6b2a3b2fc50f247
Mooooooov <mateusvasconcelos4210@gmail.com>
2020-11-29 22:23:48 -0300

    Adicionando os textos que estavam faltando

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import Text from './components/Text/Text'
 function App() {
   return (
     <>
-    <Text type="textbutton">Batatinha quente queimou</Text>
-    <Text type="textbuttonskip">Batatinha quente queimou</Text>
-    <Text type="textfinal">Batatinha quente queimou</Text>
-    <Text type="textfinal2">Batatinha quente queimou</Text>
     </>
   );
 }
