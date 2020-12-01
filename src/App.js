53775efefc478b8feb3fe737f33212fa2db38e0e
evertoont <evertoontcarmo@gmail.com>
2020-11-30 22:20:45 -0300

    Adiciona componente de Input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Text from './components/Text/Text'
+import {InputField} from './components/InputField'
 
 function App() {
   return (
-    <>
-    </>
+    <div className="App">
+    </div>
   );
 }
 
