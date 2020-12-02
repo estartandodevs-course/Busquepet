6833f34dcbbce70fe602ea27b4a07e1f4b8e55d8
Joao Paulo <joaopaulofariadecarvalho@gmail.com>
2020-12-01 21:23:49 -0300

    Removido o componente e adicionado imagens

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {Button} from './components/button/Button';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
-    <Button></Button>
   </React.StrictMode>,
   document.getElementById('root')
 );
