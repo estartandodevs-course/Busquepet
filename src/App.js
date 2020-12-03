61a3c9b6eda9c2fcdbb64aa8e2a9db32444c055b
Joao Paulo <joaopaulofariadecarvalho@gmail.com>
2020-12-02 23:51:53 -0300

    refeito button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import Icone from './assets/images/Coracao.svg';
+const { Button } = require("./components/button/Button");
+
 function App() {
   return (
     <>
+      <Button name="Vamos" icon={Icone} class="purple"></Button>
     </>
   );
 }
--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import './Button.scss';
 
 export const Button =  props => {
-
     return(
         <div>
-            <button disabled>{props.nome}<img src={props.img} alt="seta" ></img></button>
+            <button className={props.class} disabled>
+                <div className="button-Dad">
+                    <span>{props.name}</span>
+                    <img className="Icons" src={props.icon} alt="Icon"/>
+                </div>
+            </button>
         </div>
     )
 }
