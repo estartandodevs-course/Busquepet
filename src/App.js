20c16b10d6b8b8eb7402c807e432c1d4cc58a2e3
Mooooooov <mateusvasconcelos4210@gmail.com>
2020-12-04 01:08:04 -0300

    comentando linha de código que não vai ser usada agora

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import RadioButton from './components/RadioButton'
-import { useState } from 'react';
+// import RadioButton from './components/RadioButton'
+// import { useState } from 'react';
 // import { Select } from './components/select/Index';
 // import Icone from './assets/images/Coracao.svg';
-import Routes from './components/Routes';
+// import Routes from './components/Routes';
 // const { Button } = require('./components/button/Index');
 
 function App() {
-	const [person, setPerson] = useState("")
+	// const [person, setPerson] = useState("")
 
 	return (
 		<div className="App">
 
-			<Routes />
+			{/* <Routes /> */}
 			{/* <Button name="Vamos" icon={Icone} class="purple"></Button>
 			<Select type="estado" /> */}
 			{/* <RadioButton text="Pessoa Física" name="typePerson" id="Pessoa Física" onChange={setPerson} />
