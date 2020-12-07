7b3913251a4f357e0c5e3a1c871bf2236bf44af7
Mooooooov <mateusvasconcelos4210@gmail.com>
2020-12-07 16:00:42 -0300

    Concluindo a tela de cadastro do tipo de pessoa

--- src/pages/cadastro-do-doador/cadastro-pessoa-juridica/index.jsx
+++ src/pages/cadastro-do-doador/cadastro-pessoa-juridica/index.jsx
@@ -1,4 +1,8 @@
+import { useLocation } from "react-router-dom"
+
 export default function RegisterLegalPerson () {
+    const location = useLocation()
+    {console.log(location)}
     return(
         <h1>Register-Legal-Person</h1>
     )
