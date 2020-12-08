9d0ef41b93dcd42c099453a9ba729fb0efeeae7b
Breno Nunes <brenontavares@gmail.com>
2020-12-07 23:52:26 -0300

    remove redundant error

--- src/pages/cadastro-do-doador/cadastro-pessoa-juridica/index.jsx
+++ src/pages/cadastro-do-doador/cadastro-pessoa-juridica/index.jsx
@@ -1,9 +1,7 @@
-import { useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom";
 
-export default function RegisterLegalPerson () {
-    const location = useLocation()
-    {console.log(location)}
-    return(
-        <h1>Register-Legal-Person</h1>
-    )
+export default function RegisterLegalPerson() {
+  const location = useLocation();
+  console.log(location);
+  return <h1>Register-Legal-Person</h1>;
 }
