b456b464fe48b565421b31e95b5746a1cc03b3b7
Yago Valuche <valuchedevs@gmail.com>
2020-12-08 14:10:04 -0300

    concluida a interface da feature/locationdata

--- src/pages/cadastro-do-doador/cadastro-dados-localizacao/index.jsx
+++ src/pages/cadastro-do-doador/cadastro-dados-localizacao/index.jsx
@@ -1,28 +1,38 @@
 import Header from '../../../components/Header/index';
 import Text from '../../../components/Text/index';
-import {InputField} from '../../../components/InputField';
-import {Select} from '../../../components/select/Index';
+import { InputField } from '../../../components/InputField';
+import { Select } from '../../../components/select/Index';
 import Button from '../../../components/Button';
 import Check from '../../../assets/images/Check.svg';
 import './Style.scss';
-export default function LocationData () {
+import { useState } from 'react';
+export default function LocationData() {
+	const [value, setValue] = useState('');
+	return (
+		<>
+			<Header title="Você quer doar!" subTitle="Esse é o último!!!!!" />
+			<Text type="titleform">Seus Dados de Localização</Text>
 
-    return (
-        <div>
-            <Header title="Você quer doar!" subTitle="Esse é o último!!!!!"/>
-            <Text type="titleform">Seus Dados de Localização</Text>
-
-            <div className="contact-form">
-                <InputField label="Seu CEP" type="text" placeholder="00000-000"></InputField>
-                <Select typeMap="estado" label="Seu Estado"></Select>
-                <InputField label="Sua Cidade" type="text"></InputField>
-                <InputField label="Seu Endereço" type="text"></InputField>
-                <InputField label="Seu Bairro" type="text"></InputField>
-            </div>
-
-            <div className="button-skip">
-                <Button identifier="green" name="Cadastrar" icon={Check}></Button>
-            </div>
-        </div>
-    )
+			<div className="contact-form">
+				<InputField
+					label="Seu CEP"
+					type="text"
+					placeholder="0000-000"
+					onChange={setValue}
+				/>
+				<Select typeMap="estado" label="Seu Estado" />
+				<InputField label="Sua Cidade" type="text" onChange={setValue} />
+				<InputField label="Seu Endereço" type="text" onChange={setValue} />
+				<InputField label="Seu Bairro" type="text" onChange={setValue} />
+			</div>
+			<div className="button-skip">
+				<Button
+					disabled={!value}
+					identifier="green"
+					name="Cadastrar"
+					icon={Check}
+				/>
+			</div>
+		</>
+	);
 }
