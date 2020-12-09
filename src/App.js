667068beb946e2dedf2f1649ac022541dae32b20
Yago Valuche <valuchedevs@gmail.com>
2020-12-09 10:49:11 -0300

    Comentando os componentes de teste e deixando descomentando o componente de rotas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 // import DonorRegister from './pages/cadastro-do-doador/cadastro-tipo-de-pessoa';
 import './app.scss';
-import Loading from './pages/tela-de-load';
+// import Loading from './pages/tela-de-load';
 // import { useState } from 'react';
 // import { Select } from './components/select/Index';
 // import Icone from './assets/images/Coracao.svg';
-// import Routes from './components/Routes';
+import Routes from './components/Routes';
 // import LocationData from './pages/cadastro-do-doador/cadastro-dados-localizacao';
 // const { Button } = require('./components/button/Index');
 
@@ -14,10 +14,10 @@ function App() {
 
 	return (
 		<div className="App">
-			<Loading />
+			{/* <Loading /> */}
 			{/* <DonorRegister /> */}
 			{/* <LocationData/> */}
-			{/* <Routes /> */}
+			<Routes />
 			{/* <Button name="Vamos" icon={Icone} class="purple"></Button>
 			<Select type="estado" /> */}
 		</div>
