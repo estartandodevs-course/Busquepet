7f7ffc520ed15d3bb15b54f8a8204fc99c639162
Yago Valuche <valuchedevs@gmail.com>
2020-12-11 22:26:28 -0300

    resolvendo Warning e alterando a cor de borda do container-filter

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
+// import { useHistory } from 'react-router-dom';
 import Seta from '../../assets/images/seta.svg';
 import Text from '../Text/';
 import './Header.scss';
