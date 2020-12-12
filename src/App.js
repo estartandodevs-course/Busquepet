76b83f89cb9da57dff4f9b8e55c8f0005948f1b2
Joao Paulo <joaopaulofariadecarvalho@gmail.com>
2020-12-11 22:06:55 -0300

    finalizando

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-// import Routes from './components/Routes';
+import Routes from './components/Routes';
 import './app.scss';
-import DetailUrl from './pages/cadastro-do-pet/cadastro-upload-foto';
 
 
 function App() {
