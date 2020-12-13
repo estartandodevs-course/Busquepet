c79e3b7f6db5870817917121f90438cd4fffd129
Mooooooov <mateusvasconcelos4210@gmail.com>
2020-12-13 18:30:49 -0300

    Arrumando a fonte do modal e importando imagem do botao de fechar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './app.scss';
-import CadastroModal from './components/CadastroModal';
-// import Routes from './components/Routes';
+import Routes from './components/Routes';
 
 function App() {
 	return (
