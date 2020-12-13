5753a9e4ffb1cae38ba71ebed12b885fbd6b1f55
Mooooooov <mateusvasconcelos4210@gmail.com>
2020-12-13 19:28:50 -0300

    Adicionando condicional no Button para não aparecer o alt em momentos que o icone nao é utilizado

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Routes from './components/Routes';
+// import Routes from './components/Routes';
 import './app.scss';
 import './global.scss'
+import ModalPerfil from './components/ModalPerfil';
 
 function App() {
 	return (
 		<div className="App">
-			<Routes />
+			{/* <Routes /> */}
+			<ModalPerfil />
 		</div>
 	);
 }
