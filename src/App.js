a46a7a5dfcfe97d4b1cbd29db45bf7842ebaa0a5
pietra-nunes <pietratavares@gmail.com>
2020-12-14 01:00:19 -0300

    Instalando e configurando firebase

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Routes from './components/Routes';
-import './app.scss';
-
+import React from "react";
+import Routes from "./components/Routes";
+import "./app.scss";
+import "./config/firebase.config";
 
 function App() {
-	return (
-		<div className="App">
-			<Routes />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  );
 }
 
 export default App;
