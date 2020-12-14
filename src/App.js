1ba9b4c07bb0f6c0acf1a0445b97c1c41c1b98d2
Mooooooov <mateusvasconcelos4210@gmail.com>
2020-12-14 03:18:27 -0300

    Correção no arquivo app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-// import Routes from './components/Routes';
+import Routes from './components/Routes';
 import './app.scss';
 import './global.scss'
-import ModalPerfil from './components/ModalPerfil';
 
 function App() {
 	return (
 		<div className="App">
-			{/* <Routes /> */}
-			<ModalPerfil />
+			<Routes />
 		</div>
 	);
 }
