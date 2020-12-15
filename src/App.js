faa07693a2065809f53ddce37dc1ea4dc22a0773
Joao Paulo <joaopaulofariadecarvalho@gmail.com>
2020-12-15 02:23:40 -0300

    limpado tela app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from "react";
-// import Routes from "./components/Routes";
+import Routes from "./components/Routes";
 import "./app.scss";
-// import "./config/firebase.config";
+import "./config/firebase.config";
 import "./global.scss";
-// import FlowProfile00 from "./pages/perfil-doador/perfil-doador00";
-import FlowProfile01_2 from "./pages/perfil-doador/perfil-doador00/perfil-doador01.2/index";
 
 function App() {
   return (
     <div className="App">
-      <FlowProfile01_2></FlowProfile01_2>
-      {/* <FlowProfile00></FlowProfile00> */}
-      {/* <Routes /> */}
+      <Routes />
     </div>
   );
 }
