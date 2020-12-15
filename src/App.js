8936319ce3c826c1d7c0a76222f5493b7a15eeb3
Joao Paulo <joaopaulofariadecarvalho@gmail.com>
2020-12-15 02:19:34 -0300

    criado a tela profile donor 01.2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
-import Routes from "./components/Routes";
+// import Routes from "./components/Routes";
 import "./app.scss";
-import "./config/firebase.config";
+// import "./config/firebase.config";
 import "./global.scss";
+// import FlowProfile00 from "./pages/perfil-doador/perfil-doador00";
+import FlowProfile01_2 from "./pages/perfil-doador/perfil-doador00/perfil-doador01.2/index";
 
 function App() {
   return (
     <div className="App">
-      <Routes />
+      <FlowProfile01_2></FlowProfile01_2>
+      {/* <FlowProfile00></FlowProfile00> */}
+      {/* <Routes /> */}
     </div>
   );
 }
