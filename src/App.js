c3be70496108a15588568622a982933fcd5ac988
pietra-nunes <pietratavares@gmail.com>
2020-12-14 21:07:12 -0300

    WIP: adicionando o global

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Routes from "./components/Routes";
 import "./app.scss";
 import "./config/firebase.config";
+import "./global.scss";
 
 function App() {
   return (
