1c0db6e191adda931be5cfa5d368bdc399c2c14b
Mooooooov <mateusvasconcelos4210@gmail.com>
2020-12-15 03:35:46 -0300

    Adicionando o modal de sucesso de perfil

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-// import Routes from './components/Routes';
+import Routes from './components/Routes';
 import './app.scss';
 import './global.scss'
-import InfoProfile from './pages/perfil-doador/info-do-doador';
 
 function App() {
 	return (
 		<div className="App">
-			{/* <Routes /> */}
-			<InfoProfile />
+			<Routes />
 		</div>
 	);
 }
