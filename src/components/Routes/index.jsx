b011fcda5aea9ea290d27be8fc5d00f2f463cd5d
pietra-nunes <pietratavares@gmail.com>
2020-12-16 00:42:54 -0300

    Incluindo a rota para os perfis

--- src/components/Routes/index.jsx
+++ src/components/Routes/index.jsx
@@ -9,6 +9,10 @@ import DonorRegister from '../../pages/cadastro-do-doador/cadastro-tipo-de-pesso
 import ContactDetails from '../../pages/cadastro-do-doador/cadastro-dados-contato';
 import ProfilePet from '../../pages/perfil-pet';
 import Login from '../../pages/login';
+import FlowProfile01_2 from '../../pages/perfil-doador/perfil-doador00/perfil-doador01.2';
+import FlowProfile00 from '../../pages/perfil-doador/perfil-doador00';
+import InfoProfile from '../../pages/perfil-doador/info-do-doador';
+import InfoProfileEdit from '../../pages/perfil-doador/info-do-doador-edit';
 
 export default function Routes() {
 	return (
@@ -28,6 +32,10 @@ export default function Routes() {
 				<Route exact path="/cadastro-do-doador" component={DonorRegister} />
 				<Route exact path="/dados-do-contato" component={ContactDetails} />
 				<Route exact path="/perfil-do-pet" component={ProfilePet} />
+				<Route exact path="/info-do-doador" component={InfoProfile} />
+				<Route exact path="/info-edit-do-doador" component={InfoProfileEdit} />
+				<Route exact path="/perfil-do-doador00" component={FlowProfile00} />
+				<Route exact path="/perfil-do-doador01" component={FlowProfile01_2} />
 			</Switch>
 		</BrowserRouter>
 	);
