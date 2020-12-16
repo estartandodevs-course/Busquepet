f9a67595a359a731635f71fd0831512713d3f8ba
Yago Valuche <valuchedevs@gmail.com>
2020-12-16 16:05:51 -0300

    Excluindo comentarios

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import Routes from "./components/Routes";
 import "./app.scss";
 import "./config/firebase.config";
 import "./global.scss";
-// import RegisterAdopter1 from "./pages/cadastro-do-doador/cadastro-tipo-de-pessoa/adopter-registration/register-adopter-data";
 
 
 function App() {
   return (
     <div className="App">
       <Routes />
-      {/* <RegisterAdopter1 /> */}
     </div>
   );
 }
