136bc38497996fa628e8f327048637e5b53c7023
Joao Paulo <joaopaulofariadecarvalho@gmail.com>
2020-12-17 18:28:17 -0300

    Adicionado dois Card de Gato

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 
 import React from "react";
-// import Routes from "./components/Routes";
+import Routes from "./components/Routes";
 import "./app.scss";
-// import "./config/firebase.config";
+import "./config/firebase.config";
 import "./global.scss";
-import Feed from "./pages/feed";
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Routes /> */}
-      <Feed></Feed>
+      <Routes />
     </div>
   );
 }
