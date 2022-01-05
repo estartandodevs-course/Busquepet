12f1b24c5fba15dc1d32bfe2f188f2a511b31aac
Mateus Vasconcelos <mateusvasconcelos4210@gmail.com>
2022-01-04 21:43:04 -0300

    📦 NEW: Adding the component files

---
+++ src/components/Header/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import Seta from "assets/images/seta.svg";
+import Text from "Text/";
+import "./styles.scss";
+
+export default function Header({ title, subTitle }) {
+  const { goBack } = useHistory();
+
+  function comeBack() {
+    goBack();
+  }
+
+  return (
+    <header className="header">
+      <img onClick={comeBack} className="arrow" src={Seta} alt="seta" />
+      <div className="textform">
+        <Text className="titulo" type="titleheader">
+          {title}
+        </Text>
+        <Text type="subtitleheader">{subTitle}</Text>
+      </div>
+    </header>
+  );
+}
---
+++ src/components/RadioButton/index.js
@@ -0,0 +1,20 @@
+import Text from "../Text";
+import "./RadioButton.scss";
+
+const RadioButton = ({ text, name, id, onChange }) => (
+  <label
+    className="radioContainer"
+    htmlFor={id}
+    onChange={(event) => onChange(event.target.id)}
+  >
+    <input type="radio" name={name} id={id} />
+    <div className="circle-area">
+      <span className="circle" />
+    </div>
+    <div>
+      <Text type="textform">{text}</Text>
+    </div>
+  </label>
+);
+
+export default RadioButton;
