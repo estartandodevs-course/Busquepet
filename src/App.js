94c562e95123b18ae3cbe41f8c495533744b094f
Mateus Vasconcelos <mateusvasconcelos4210@gmail.com>
2022-01-06 20:10:20 -0300

    📦 NEW: Finishing the Adopter screens

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 // import Routes from "components/Routes";
 import "config/firebase.config";
 import "./global.scss";
-import Feed from "screens/Feed";
+import AdopterLocation from "screens/AdopterLocation";
 
 function App() {
   return (
     <div className="App">
-      <Feed />
+      <AdopterLocation />
     </div>
   );
 }
---
+++ src/screens/AdopterLocation/index.js
@@ -0,0 +1,90 @@
+import React, { useState } from "react";
+import { Select, Button, Text, InputField, Header } from "components";
+import Check from "assets/images/Check.svg";
+import Seta from "assets/images/seta.svg";
+import "./styles.scss";
+
+export default function RegistratLocation(props) {
+  const { setValue, onSubmit } = props;
+
+  const [cepAdopter, setCepAdopter] = useState("");
+  const [stateAdopter, setStateAdopter] = useState("");
+  const [city, setCity] = useState("");
+  const [addressAdopter, setAddress] = useState("");
+  const [district, setDistrict] = useState("");
+
+  return (
+    <>
+      <div className="cabeça">
+        <div className="profile">
+          <img src={Seta} alt="perfil"></img>
+        </div>
+        <div className="textcabeça">
+          <Text className="titulo" type="titleheader">
+            Você quer adotar!
+          </Text>
+          <Text type="subtitleheader">Só mais essa, tá?!</Text>
+        </div>
+      </div>
+      <Text type="titleform">Seus Dados de Localização</Text>
+
+      <section className="container-form-location">
+        <div className="contact-form">
+          <InputField
+            id="adotante"
+            label="Seu CEP"
+            type="text"
+            placeholder="0000-000"
+            onChange={(event) => setCepAdopter(event) || setValue("Cep", event)}
+          />
+          <Select
+            id="labelRoxo"
+            identifier="roxo"
+            typeMap="estado"
+            label="Seu Estado"
+            onChange={(event) =>
+              setStateAdopter(event) || setValue("State", event)
+            }
+          />
+          <InputField
+            label="Sua Cidade"
+            type="text"
+            id="adotante"
+            onChange={(event) => setCity(event) || setValue("City", event)}
+          />
+          <InputField
+            label="Seu Endereço"
+            type="text"
+            id="adotante"
+            onChange={(event) =>
+              setAddress(event) || setValue("Address", event)
+            }
+          />
+          <InputField
+            label="Seu Bairro"
+            type="text"
+            id="adotante"
+            onChange={(event) =>
+              setDistrict(event) || setValue("District", event)
+            }
+          />
+        </div>
+      </section>
+      <div className="button-skip">
+        <Button
+          disabled={
+            !cepAdopter ||
+            !stateAdopter ||
+            !city ||
+            !addressAdopter ||
+            !district
+          }
+          className="purple"
+          name="Cadastrar"
+          icon={Check}
+          onClick={onSubmit}
+        />
+      </div>
+    </>
+  );
+}
